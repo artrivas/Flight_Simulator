fbd159fdcef908bc8bc0f70bf42145079270ea42
diegorivadeneyra <diego.rivadeneyra@utec.edu.pe>
2024-07-04 20:25:03 -0500

    sonidos y manejo de las ruedas

---
+++ Assets/Scripts/Ruedas.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ruedas : MonoBehaviour
+{
+    public GameObject[] objects;
+
+    // Esta función será llamada cuando el evento de MaximumReached se dispare
+    public void DeactivateObjects()
+    {
+        foreach (GameObject obj in objects)
+        {
+            obj.SetActive(false);
+        }
+    }
+
+    // Esta función será llamada cuando necesites activar los objetos
+    public void ActivateObjects()
+    {
+        foreach (GameObject obj in objects)
+        {
+            obj.SetActive(true);
+        }
+    }
+}
