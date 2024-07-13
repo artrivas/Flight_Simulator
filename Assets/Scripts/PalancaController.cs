38838d498f5446ca6d274f599c3e737828f2b9e2
Juan <juan.torres.f@utec.edu.pe>
2024-07-13 17:12:11 -0500

    PalancaController - Diego

--- Assets/Scripts/PalancaController.cs
+++ Assets/Scripts/PalancaController.cs
@@ -4,30 +4,36 @@ using UnityEngine;
 
 public class PalancaController : MonoBehaviour
 {
-    private float velocidadMaxima = 30f;
+    private float velocidadMaxima = 35f;
     public GameObject jet;
     private float anguloActual = 0f; // Ángulo actual de la palanca
     float anguloPalanca;
-    float velocidadActual=0f;
-    float factorVelocidad = 15f;
+    float velocidadActual = 0f;
+    float factorVelocidad = 5f;
     // Método para obtener el ángulo actual de la palanca
     void FixedUpdate()
     {
         // Obtener el ángulo actual de la palanca
         anguloPalanca = anguloActual;
         // Calcular la velocidad en función del ángulo de la palanca
-        velocidadActual += anguloPalanca * factorVelocidad;
+        if (anguloPalanca == 0)
+        {
+            velocidadActual += -0.5f * factorVelocidad;
+        }
+        else
+        {
+            velocidadActual += anguloPalanca * factorVelocidad;
+        }
+
 
         // Limitar la velocidad para que no supere la velocidad máxima
         //velocidadActual = Mathf.Clamp(velocidadActual, 0f, velocidadMaxima);
-        if (velocidadActual > velocidadMaxima) {
+        if (velocidadActual > velocidadMaxima)
+        {
             velocidadActual = velocidadMaxima;
         }
-        Debug.Log(velocidadActual);
+        else if (velocidadActual < 0f) { velocidadActual = 0f; }
 
-
-        // Aquí podrías actualizar la posición del avión basado en velocidadActual
-        // Por ejemplo:
         // transform.Translate(Vector3.forward * velocidadActual * Time.deltaTime);
         jet.transform.Translate(Vector3.forward * velocidadActual * Time.deltaTime);
         //transform.Translate(Vector3.forward * velocidadActual * Time.deltaTime);
@@ -39,11 +45,11 @@ public class PalancaController : MonoBehaviour
     }
     public void midVel()
     {
-        anguloActual = 1.5f;
+        anguloActual = 0.5f;
     }
 
     public void maxVel()
     {
-        anguloActual = 3f;
+        anguloActual = 1.5f;
     }
 }
